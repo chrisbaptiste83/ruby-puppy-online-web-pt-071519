dacb8ea8815c4b3f51ec82b4b3aff90f5e8e2014
Christopher Baptiste <chrisbaptiste83@gmail.com>
2019-08-04 05:02:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,7 +2,7 @@ class Dog
   @@all =[]
   def initialize(name)
     @name = name
-    
+    save 
   end 
     
     def self.all 
@@ -18,5 +18,6 @@ class Dog
  end 
  
  def save 
-   
+   @@all << self
+ end 
 end
